192c107da550a96c2f85e2a5dcb8855976327981
Tech-Recruiter-Justin <justin7623@gmail.com>
2021-01-24 00:22:18 +0800

    YelpCamp 405

---
+++ app.js
@@ -0,0 +1,43 @@
+// requiring all npm packages
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campgrounds')
+
+// set up mongo and mongoose connection path
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+// initiate mongodb connection, throws an error when fail
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+// initiate express app
+const app = express();
+
+// enable ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+// testing if ejs is working
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+// to create a new campground
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'Backyard', description: 'cheap camping!' });
+    await camp.save();
+    res.send(camp);
+})
+
+// open express on port 3000
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
