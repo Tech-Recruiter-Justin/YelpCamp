ebe39703a25d4fa45e4262def37bb990ceb812c6
Justin Choi <justin7623@gmail.com>
2021-05-06 16:34:44 +0800

    added seeding module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campgrounds');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -16,12 +17,17 @@ db.once("open", () => {
 
 const app = express();
 
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'Home Backyard', description: 'cheap camping!'});
+    await camp.save();
+    res.send(camp);
 });
 
 app.listen(3000, () => {
